fd75cb925d5f870bf618f5a90d5ab592de768500
danibex <danielivam96@gmail.com>
2023-03-29 18:57:27 -0300

    .

--- js/gridCourses.js
+++ js/gridCourses.js
@@ -3,16 +3,24 @@ $("#btn-ead").click(function (e) {
     $('.presencial').fadeOut(600)
     $(".ead").fadeIn(600)
     $("")
+    $('#btn-ead').addClass('active')
+    $("#btn-presencial").removeClass('active')
+    $("#btn-todos").removeClass('active')
    });
-   
    $("#btn-presencial").click(function (e) {
     e.preventDefault()
     $(".ead").fadeOut(600)
     $(".presencial").fadeIn(600)
+    $("#btn-presencial").addClass('active')
+    $('#btn-ead').removeClass('active')
+    $("#btn-todos").removeClass('active')
    });
-   $('#btn-todos').click(function(e) {
+   $("#btn-todos").click(function(e) {
      e.preventDefault()
      $(".presencial").fadeIn(600)
      $(".ead").fadeIn(600)
+     $("#btn-todos").addClass('active')
+     $("#btn-presencial").removeClass('active')
+     $('#btn-ead').removeClass('active')
    })
    
